61920f22bdf89b54b3b7d51116607ee845a49f68
alexandriax <166084006+alexandriax@users.noreply.github.com>
2025-04-20 15:52:18 -0400

    add login/logout views & functionality

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,15 +1,44 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView
 from .models import Recipe
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipes/recipes_home.html', {'recipes': recipes})
 
+@login_required
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
 
+@login_required
 def recipe_detail(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
     return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
+
+def login_view(request):
+    error_message = None
+    form = AuthenticationForm()
+
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('recipes:recipe_list')
+            else:
+                error_message = 'Invalid credentials, try again.'
+        else:
+            error_message = 'Form error, please check your input.'
+
+    return render(request, 'auth/login.html', {'form': form, 'error_message': error_message})
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'auth/logout.html')
--- src/settings.py
+++ src/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,6 +130,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login/'
+
 
 
 
