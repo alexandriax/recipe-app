cb7eb7a69485dd1d26ef6535b813b14903835875
alexandriax <166084006+alexandriax@users.noreply.github.com>
2025-04-27 18:03:29 -0400

    Fix import paths for recipes app

--- src/urls.py
+++ src/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from recipes.views import login_view, logout_view
+from src.recipes.views import login_view, logout_view
 
 
 urlpatterns = [
